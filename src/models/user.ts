aebe58d58bef3085dcfcd29b9f4c74517a352938
Dafianeseif <dafiane@fss.u-sfax.tn>
2025-02-17 00:17:28 +0100

    first commit

---
+++ src/models/user.ts
@@ -0,0 +1,31 @@
+import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    __id:{ ObjectId
+        
+    },
+    auth0Id: {
+        type    : String,
+        required: true,
+    },
+    email : {
+        type    : String,
+        required: true,
+    },
+    name  : {
+        type    : String,
+    },
+    adressLine1: {
+        type    : String,
+    },
+    city : {
+        type    : String,
+    },
+    country : {
+        type    : String,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
